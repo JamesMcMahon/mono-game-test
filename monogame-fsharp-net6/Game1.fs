417ec51f080b95815bbf32e0598cfbbacd48451b
James McMahon <05.cyclone.misery@icloud.com>
2022-11-01 21:37:51 -0500

    Reformat F# file

--- monogame-fsharp-net6/Game1.fs
+++ monogame-fsharp-net6/Game1.fs
@@ -2,44 +2,45 @@
 
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
-open Microsoft.Xna.Framework.Content
 
-type Game1 () as x =
+type Game1() as x =
     inherit Game()
- 
+
     do x.Content.RootDirectory <- "Content"
     let graphics = new GraphicsDeviceManager(x)
-    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    let mutable spriteBatch =
+        Unchecked.defaultof<SpriteBatch>
 
     override x.Initialize() =
-    
+
         spriteBatch <- new SpriteBatch(x.GraphicsDevice)
         base.Initialize()
-         
-         // TODO: Add your initialization logic here
+
+        // TODO: Add your initialization logic here
 
         ()
 
     override this.LoadContent() =
-        
-         // TODO: use x.Content to load your game content here   
-         // On Windows you can load any PNG file directly as Texture2D
 
-         // Read more about MonoGame's Content Pipeline: https://docs.monogame.net/articles/tools/mgcb_editor.html
-         // or install it with package manager console: [dotnet tool install -g dotnet-mgcb-editor]
-        
-        ()
- 
-    override this.Update (gameTime) =
+        // TODO: use x.Content to load your game content here
+        // On Windows you can load any PNG file directly as Texture2D
 
-         // TODO: Add your update logic here
-        
+        // Read more about MonoGame's Content Pipeline: https://docs.monogame.net/articles/tools/mgcb_editor.html
+        // or install it with package manager console: [dotnet tool install -g dotnet-mgcb-editor]
+
         ()
- 
-    override this.Draw (gameTime) =
+
+    override this.Update(gameTime) =
+
+        // TODO: Add your update logic here
+
+        ()
+
+    override this.Draw(gameTime) =
 
         x.GraphicsDevice.Clear Color.CornflowerBlue
-        
+
         // TODO: Add your drawing code here
 
         ()
